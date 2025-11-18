b74f3a115cc506c73415dfacaf70dcf27d697d06
lukaszgrudnik <lukasz.grudnik@outlook.com>
2025-11-18 19:57:18 +0100

    fix: fix jest configuration

--- specs/shell/app.spec.tsx
+++ specs/shell/app.spec.tsx
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../../apps/shell/src/app/app';
 
-test('Renders the main page', () => {
-  const testMessage = 'App Component';
+test('renders hello', () => {
   render(<App />);
-  expect(screen.getByText(testMessage)).toBeInTheDocument();
+  expect(screen.getByText('Hello')).toBeInTheDocument();
 });
